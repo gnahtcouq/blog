7478e2712100d3497eed230715061a2812e65e78
Nomi <38210249+nomi-san@users.noreply.github.com>
2020-02-24 23:42:20 +0700

    add GA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const title = 'Nomi-san';
 const author = 'Nomi';
+const ga_key = 'UA-119524229-1';
 
 const navs = [
     {'home': '/'},
@@ -161,6 +162,14 @@ document.addEventListener('DOMContentLoaded', function()
     }
     
     fix_scale(document);
+    
+    // GA track
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', ga_key, 'auto');
+    ga('send', 'pageview');
 });
 
 function create_elm(tag = '', klass = '', inner = '') {
